ec9cb408a0a9aeaf0c93db85225b3b8b06cecdf6
AYUSHI YADAV <128875970+ayuGit11@users.noreply.github.com>
2024-04-29 15:57:57 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,15 +10,33 @@ const LoginLayout = styled.main`
   align-content: center;
   justify-content: center;
   gap: 3.2rem;
-  background-color: var(--color-grey-50);
+  background-color: var(--color-grey-200);
+`;
+
+const LoginContainer = styled.div`
+  background-color: #9acce2;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 10px rgba(204, 251, 74, 0.1);
+`;
+
+const Container = styled.div`
+  background-color: #386b85;
+  padding: 6rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 10px rgba(204, 251, 74, 0.1);
 `;
 
 function Login() {
   return (
     <LoginLayout>
-      <Logo />
-      <Heading as="h4">Log in to your account</Heading>
-      <LoginForm />
+      <Container>
+        <Logo />
+        <LoginContainer>
+          <Heading as="h4">Log in to your account</Heading>
+          <LoginForm />
+        </LoginContainer>
+      </Container>
     </LoginLayout>
   );
 }
